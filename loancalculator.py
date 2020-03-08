22ebbb9f93c330674565b4572bd3e814c12f8ec9
Ella016 <61168562+Ella016@users.noreply.github.com>
2020-03-08 16:23:04 +0200

    create calculator

---
+++ loancalculator.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+Loan = input('please input the cost of loan')
+Interest = input('please input interest')
+Years = input('please input your years')
+
+Payments = float(Loan) + float(Interest) + float(Years)
+
+print(Payments)
